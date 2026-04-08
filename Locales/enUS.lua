1232feaa6623da9bcc6b2af6e5e1078683e3333d
vstraydogstrutv <48873918+vstraydogstrutv@users.noreply.github.com>
2026-04-08 01:44:12 -0400

    Delete Locales directory

--- Locales/enUS.lua
+++
@@ -1,8 +0,0 @@
--- OneWoW Addon File
--- OneWoW_CatalogData_Quests/Locales/enUS.lua
--- Created by MichinMuggin (Ricky)
-local addonName, ns = ...
-
-ns.L = {
-    ["ADDON_LOADED"] = "OneWoW CatalogData: Quest data loaded.",
-}
